3a275c07a0d09664741b4084fee214c8aeecd0d6
renqiang <renqiang@dev.com>
2024-02-21 21:21:10 +0800

    1

--- src/views/admin/index.vue
+++ src/views/admin/index.vue
@@ -2,14 +2,15 @@
   <div>
     <div class="top flex">
       <div class="top_left">
-        <span>SexCat</span>
-        <el-popover :width="400">
-          <template #reference>
-            <span>照片作品</span>
-            
-          </template>
-          <div>okk</div>
-        </el-popover>
+        <el-button class="transparent_btn logo">SexCat</el-button>
+        <el-button class="transparent_btn">照片作品</el-button>
+        <div class="menu">
+          <div class="menu_left">
+            <p>热门作品</p>
+            <p>最新作品</p>
+          </div>
+        </div>
+        <el-button class="transparent_btn">Coser</el-button>
       </div>
       <div class="top_right"></div>
     </div>
@@ -31,10 +32,53 @@
   justify-content: space-between;
   .top_left {
     margin-left: 20px;
-    span {
-      font-size: 50px;
+    margin-right: 30px;
+    .logo {
+      font-size: 26px;
       color: #063fe1;
+      font-weight: 600;
     }
+  }
+}
+.menu {
+  display: flex;
+  justify-content: space-between;
+  padding-left: 20px;
+  padding-top: 20px;
+  width: 500px;
+  // display: none;
+  position: absolute;
+  z-index: 1;
+  top: 80px;
+  left: 100px;
+  border-radius: 12px;
+  box-shadow: 0px 10px 70px rgba(0, 0, 0, 0.15);
+  .menu_left {
+    p {
+      margin-bottom: 30px;
+      color: #0d0c22;
+      font-family: "Haas Grot Text R Web", "Helvetica Neue", Helvetica, Arial,
+        sans-serif;
+      font-size: 14px;
+      font-weight: 500;
+      line-height: 20px;
+    }
+  }
+}
+
+.transparent_btn {
+  border: none;
+  color: #6e6d7a;
+  font-weight: 600;
+  font-size: 14px;
+  &:hover {
+    background-color: transparent;
+    color: black;
+    font-size: 18px;
+    margin-left: 0px;
+  }
+  &:focus {
+    background-color: transparent;
   }
 }
 </style>
